bc0392935b82e3b71f8d1d6cb13a85b93443efed
shokkun47 <shoki1310@gmail.com>
2024-10-12 03:22:32 +0900

    complete responsible design

--- src/components/Widget/Widgets.js
+++ src/components/Widget/Widgets.js
@@ -1,18 +1,32 @@
 import { Search } from '@mui/icons-material';
-import React from 'react';
+import React, { useState } from 'react';
 import './Widgets.css';
 import Ranking from './Ranking';
 
 function Widgets() {
-  return (
-    <div className='widgets'>
-      <div className='widgets--input'>
-        <Search className='widgets--searchIcon' />
-        <input placeholder='キーワード検索' type='text' />
-      </div>
+  const [isWidgetVisible, setIsWidgetVisible] = useState(false);
 
-      <div className='widgets--widgetContainer'>
-        <Ranking />
+  const handleToggleWidget = () => {
+    setIsWidgetVisible(!isWidgetVisible);
+  };
+
+  return (
+    <div className='widgets-container'>
+      {/* スマホ用トグルボタン */}
+      <button className='widgets-toggleButton' onClick={handleToggleWidget}>
+        {isWidgetVisible ? 'ホーム' : 'ランキング'}
+      </button>
+
+      {/* Widgetsコンポーネント */}
+      <div className={`widgets ${isWidgetVisible ? 'show' : 'hide'}`}>
+        <div className='widgets--input'>
+          <Search className='widgets--searchIcon' />
+          <input placeholder='キーワード検索' type='text' />
+        </div>
+
+        <div className='widgets--widgetContainer'>
+          <Ranking />
+        </div>
       </div>
     </div>
   );
